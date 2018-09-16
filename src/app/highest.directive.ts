604b737060895011fd20e79e5bf1c6e8d4f1f46a
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-09-16 13:25:17 +0300

    Background image added

--- src/app/highest.directive.ts
+++ src/app/highest.directive.ts
@@ -7,18 +7,28 @@ export class HighestDirective {
 
   constructor(private elem: ElementRef) {}
 
-  @HostListener("click") onClicks(){
-  this.textDeco("yellow")
-}
-@HostListener("dblclick") onDoubleClicks(){
-  this.textDeco("red")
-}
+//   @HostListener("click") onClicks(){
+//   this.textDeco("yellow")
+// }
+// @HostListener("dblclick") onDoubleClicks(){
+//   this.textDeco("red")
+// }
 // $scope.max = Math.max.apply(Math,$scope.data.map(function(quote){
 //   return quote.like;
 // }));
   // Math.max(quotes.like){
   //   this.textDeco("line-through")
   // }
+
+  // 
+  //   @HostListener() highestLike(){
+  //     let max:number;
+  //     max= 5;
+  //     if(this.quote.like>max){
+  //       this.textDeco("yellow")
+  //     }
+  // }
+
   private textDeco(action:string){
     this.elem.nativeElement.style.backgroundColor = action;
 
